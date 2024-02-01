5c665899af40aaf072ad2cf17210d65367fa86b4
rafael <rafaelmarinhoalves5@gmail.com>
2024-02-01 20:06:05 -0300

    initial commit

---
+++ src/main/java/com/Cdv/ControleVendas/ControleVendasApplication.java
@@ -0,0 +1,13 @@
+package com.Cdv.ControleVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Cdv/ControleVendas/ControleVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cdv.ControleVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
